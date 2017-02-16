41db7f5a7834bd3eefb0b2ef499b96b42d38bf66
ade-sede <ade-sede@ade-sede-arch.localdomain>
2017-02-16 15:35:46 +0100

    Uploading env

---
+++ zsh/zsh_prompt.zsh
@@ -0,0 +1,52 @@
+# Inspired from agnoster theme from oh my zsh utility
+# This version is actually just taken straight from it to do the minimal job i want
+# I'll implement my own after studying this one
+# NOTE : I managed to build a prompt that does the same job when it comes to 
+# displaying bars like this, but this implementation allows me to add features
+# so i'll keep it for now
+
+# Setting up a few vars
+CURRENT_BG='NONE'
+PRIMARY_FG=black
+SEGMENT_SEPARATOR="\ue0b0"
+
+prompt_segment() {
+  local bg fg
+  [[ -n $1 ]] && bg="%K{$1}" || bg="%k"
+  [[ -n $2 ]] && fg="%F{$2}" || fg="%f"
+  if [[ $CURRENT_BG != 'NONE' && $1 != $CURRENT_BG ]]; then
+    print -n "%{$bg%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR%{$fg%}"
+  else
+    print -n "%{$bg%}%{$fg%}"
+  fi
+  CURRENT_BG=$1
+  [[ -n $3 ]] && print -n $3
+}
+
+prompt_end() {
+  if [[ -n $CURRENT_BG ]]; then
+    print -n "%{%k%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR"
+  else
+    print -n "%{%k%}"
+  fi
+  print -n "%{%f%}"
+  CURRENT_BG=''
+}
+
+prompt_context() {
+  local user=`whoami`
+
+  if [[ "$user" != "$DEFAULT_USER" || -n "$SSH_CONNECTION" ]]; then
+    prompt_segment red $PRIMARY_FG " %(!.%{%F{yellow}%}.)$user "
+  fi
+}
+
+prompt_dir() {
+  prompt_segment blue black ' %~ '
+}
+
+echo_prompt() {
+	prompt_context
+	prompt_dir
+	prompt_end
+}
